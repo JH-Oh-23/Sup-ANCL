d5b58e753a52b4f8713119d066f1d8ddae7f8681
JH-Oh-23 <jeongheon_oh@bok.or.kr>
2024-06-08 16:29:00 +0900

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 import argparse
 import os
 from prettytable import PrettyTable
